53b141423fb290c1b8d1637e17f453b9e4687672
juliataro <julia.taro@gmail.com>
2022-01-20 13:34:27 +0200

    first commit

--- client/login-sys/src/App.js
+++ client/login-sys/src/App.js
@@ -1,37 +1,117 @@
-import './App.css';
-import {useState} from "react";
+// https://www.youtube.com/watch?v=3K347USUIm8&list=PLVfMKQXDAhGURizWJ7EGvJfvYO0rGap_z&index=6
 
+import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+import Axios from "axios";
+import { Button } from "react-bootstrap";
 
 function App() {
-
   // Variables to work with frontend data
-  const [name, setName] = useState("")
-  const [age, setAge] = useState(0)
-  const [country, setCountry] = useState("")
-  const [position, setPosition] = useState("")
-  const [wage, setWage] = useState(0)
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
 
-  // const displayInfo = () => {
-  //   console.log(name + age + country + position + wage)
-  // };
+  // Hook for displaying data from DB, and will store our information in a list
+  const [employeeList, setEmployeeList] = useState([]);
+
+  // Axios to make POST request funktion on Add employee Button
+  // And sending object with budy, in which properties> name..
+  const addEmployee = () => {
+    Axios.post("http://localhost:3003/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      console.log("success");
+    });
+  };
+
+  // Function to get employees from DB
+  const getEmployees = () => {
+    Axios.get("http://localhost:3003/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
 
   // Frontend part and each input has a hook to catch an event
   return (
     <div className="App">
       <div className="information">
-      <label>Name:</label>
-      <input type="text" onChange={(event) => {setName(event.target.value);}}/>
-      <label>Age:</label>
-      <input type="number" onChange={(event) => {setName(event.target.value);}} />
-      <label>Country:</label>
-      <input type="text" onChange={(event) => {setName(event.target.value);}} />
-      <label>Position</label>
-      <input type="text" onChange={(event) => {setName(event.target.value);}} />
-      <label>Wage (year):</label>
-      <input type="number" onChange={(event) => {setName(event.target.value);}} />
+        <label>Name:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Age:</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setAge(event.target.value);
+          }}
+        />
+        <label>Country:</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setCountry(event.target.value);
+          }}
+        />
+        <label>Position</label>
+        <input
+          type="text"
+          onChange={(event) => {
+            setPosition(event.target.value);
+          }}
+        />
+        <label>Wage (year):</label>
+        <input
+          type="number"
+          onChange={(event) => {
+            setWage(event.target.value);
+          }}
+        />
 
-      {/* <button onClick={displayInfo}>Add Employee</button> */}
-      <button>Add Employee</button>
+        {/* <button onClick={displayInfo}>Add Employee</button> */}
+        <button variant="primary" onClick={addEmployee}>
+          Add Employee
+        </button>
+        <Button variant="primary">Primary</Button>
+      </div>
+
+      {/* The line before the data fetched from DB */}
+
+      <hr
+        style={{
+          color: "#ed1515",
+          backgroundColor: "#ed1515",
+          height: 0.5,
+          marginTop: "30px",
+          width: 300 + "px",
+        }}
+      />
+
+      {/* Button for showing employees */}
+      <div className="employees">
+        <button onClick={getEmployees}>Show employees</button>
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee">
+              <h3>Name: {val.name}</h3>
+              <h3>Age: {val.age}</h3>
+              <h3>Country: {val.country}</h3>
+              <h3>Position: {val.position}</h3>
+              <h3>Wage: {val.wage}</h3>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,42 +1,66 @@
-const express = require('express');
+// node index.js - command to run express in browser
+
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
+const cors = require("cors");
+const mysql = require("mysql2");
+
+// Allowing to make calls from frontend to backand api
+app.use(cors());
+app.use(express.json());
 
 // Constant for DabaBase
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "juliataro0891",
-    database: "employeeSystem"
+  user: "root",
+  host: "localhost",
+  password: "juliataro0891",
+  database: "employee_sys",
 });
 
 /**
-*  Post new user route with req res variables and in
+*  POST request new user route with (req, res) variables and in
 function variables that we take from frontend
-*/ 
-app.post('/create', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    const country = req.body.country;
-    const position = req.body.position;
-    const wage = req.body.wage;
+*/
+app.post("/create", (req, res) => {
+  const name = req.body.name;
+  const age = req.body.age;
+  const country = req.body.country;
+  const position = req.body.position;
+  const wage = req.body.wage;
 
-    // Inserting data into DB
-    db.query(
-        'INSERT INTO empleeys (name, age, country, position, wage) VALUES (?,?,?,?,?)', 
-        [name, age, country, position, wage], 
-        (err, result) => {
-            if(err) {
-                console.error(err);
-            } else {
-                res.send("Values Inserted");
-            }
-        }
-    );
+  // Inserting data into DB
+  db.query(
+    "INSERT INTO employee (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.send("Values Inserted");
+      }
+    }
+  );
 });
 
+/**
+*  TODO display employees from DB with GET request route with (req, res) variables and in
+function variables that we take from frontend, 
+*/
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employee", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
 // Listening for port cheking if serfer running, in terminal command: run indexjs
-app.listen(3001, () =>{
-    console.log('your server is runnig on port 3001')
-})
+app.listen(3003, () => {
+  console.log("your server is runnig on port 3003");
+  db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+});
